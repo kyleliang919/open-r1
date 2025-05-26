d004e6cf0ca17408c7be907d47bce7c3f866baf9
Kaizhao Liang <kyleliang919@gmail.com>
2025-05-26 14:58:31 -0500

    Update grpo_cautious.py

--- src/open_r1/grpo_cautious.py
+++ src/open_r1/grpo_cautious.py
@@ -110,7 +110,7 @@ def main(script_args, training_args, model_args):
     # Initialize the GRPO trainer
     #############################
     optimizer = AdamW(model.parameters(), lr = training_args.learning_rate, betas = (training_args.adam_beta1, training_args.adam_beta2), weight_decay=training_args.weight_decay)
-    num_training_steps = len(dataset[script_args.dataset_train_split]) // (training_args.world_size * training_args.per_device_train_batch_size * training_args.gradient_accumulation_steps)
+    num_training_steps = len(dataset[script_args.dataset_train_split]) // (training_args.world_size * training_args.gradient_accumulation_steps)
     scheduler = get_cosine_schedule_with_warmup(optimizer, int(training_args.warmup_ratio * num_training_steps), num_training_steps, last_epoch = -1)
     trainer = GRPOTrainer(
         model=model,
